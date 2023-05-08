187224f2adbfe7fd03528c1701d9a9169af85a99
Stefan Rotaru <stefanrotaru@hotmail.com>
2023-05-08 23:09:56 +0300

    Working on a form to send the order to the backend.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,70 +1,82 @@
-import { useContext, useState } from 'react';
+import { useContext, useState } from "react";
 
-import Modal from '../UI/Modal';
-import CartItem from './CartItem';
-import classes from './Cart.module.css';
-import CartContext from '../../store/cart-context';
-import ConfettiExplosion from 'react-confetti-explosion';
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import ConfettiExplosion from "react-confetti-explosion";
+import Checkout from "./Checkout";
 
-// function App() {
-//   const [isExploding, setIsExploding] = React.useState(false);
-//   return <>{isExploding && <ConfettiExplosion />}</>;
-// }
+const Cart = (props) => {
+  const [isExploding, setIsExploding] = useState(false);
+  const [isCheckout, setIsCheckout] = useState(false);
+  const cartCtx = useContext(CartContext);
 
-const Cart = props => {
-    const [isExploding, setIsExploding] = useState(false);
-    const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
 
-    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-    const hasItems = cartCtx.items.length > 0;
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-    const cartItemRemoveHandler = id => {
-        cartCtx.removeItem(id)
-    };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
-    const cartItemAddHandler = item => {
-        cartCtx.addItem({...item, amount: 1})
-    };
+  const confettiHandler = () => {
+    if (isExploding === false) {
+      setIsExploding(true);
+    //   cartCtx.removeAll();
+      setTimeout(() => {
+        setIsExploding(false);
+      }, 2000);
+    }
+  };
 
-    const cartItems = (
-        <ul className={classes['cart-items']}>
-            {cartCtx.items.map(item => (
-                <CartItem 
-                    key={item.id} 
-                    name={item.name}
-                    price={item.price}
-                    amount={item.amount}
-                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
-                    onAdd={cartItemAddHandler.bind(null, item)}
-                />
-            ))}
-        </ul>
-    );
+  const orderHandler = () => {
+    setIsCheckout(true);
+    confettiHandler();
+  };
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
 
-    const onOrderBtnHandler = () => {
-        if (isExploding === false) {
-            setIsExploding(true);
-            cartCtx.removeAll();
-            setTimeout(() => {
-                setIsExploding(false);
-            }, 2000)
-        }
-    };
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
 
-    return (
-        <Modal onClose={props.onClose}>
-            {isExploding && <ConfettiExplosion />}
-            {cartItems}
-            <div className={classes.total}>
-                <span>Total Amount</span>
-                <span>{totalAmount}</span>
-            </div>
-            <div className={classes.actions}>
-                <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-                {hasItems && <button className={classes.button} onClick={onOrderBtnHandler}>Order</button>}
-            </div>
-        </Modal>
-    )
+  return (
+    <Modal onClose={props.onClose}>
+      {isExploding && <ConfettiExplosion />}
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {!isCheckout && modalActions}
+    </Modal>
+  );
 };
 
 export default Cart;
